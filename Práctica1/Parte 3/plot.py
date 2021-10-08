9bf7bcd701809ac19fe5fffe68051172a192d213
José Antonio Álvarez <joseantonioao32@gmail.com>
2021-10-08 12:15:53 +0200

    Añadiendo gráficas de la parte 3

--- Práctica1/Parte 3/plot.py
+++ Práctica1/Parte 3/plot.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
 import math
+import pylab
 
 def read_players(file="output-hadoop.txt"):
 	with open(file, 'r') as f:
@@ -23,14 +24,20 @@ def read_players(file="output-hadoop.txt"):
     
 def plot_normal(mu, var, color):
 	sigma = math.sqrt(var)
-	x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
-	plt.plot(x, stats.norm.pdf(x, mu, sigma), color)
+	x = np.linspace(10, 40, 100)
+	plt.plot(x, stats.norm.pdf(x, mu, sigma))
 
+def color_generator(n_colors):
+	cm = pylab.get_cmap('gist_rainbow')
+	return (cm(1.*i/n_colors) for i in range(n_colors))
 
 data = read_players()
-colors = ["r", "b", "g", "y", "orange"]
-for team, c in zip(data.values(), colors):
-	print(team)
-	plot_normal(team[0], team[1], c)
+color_gen = color_generator(len(data))
+
+plt.figure(figsize=(18, 10))
+for team_stats in data.values():
+	plot_normal(team_stats[0], team_stats[1], next(color_gen))
+plt.legend(data.keys())
+plt.title("Comparativa de edades en los distintos equipos")
+plt.savefig('spark.png')
 plt.show()
-plt.savefig('foo.png')
